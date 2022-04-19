117e19a67e512e0efae7ef767af01a1204ab8946
Doctor-Sobeck <73001976+Doctor-Sobeck@users.noreply.github.com>
2022-04-19 13:04:06 +1200

    Create sqrt.py

---
+++ sqrt.py
@@ -0,0 +1,17 @@
+def sqrt(n):
+    x = n / 2
+    shift = x / 2
+    
+    i = 0
+    # Number of iterations defines accuracy
+    maxIterations = 60
+    while (x**2 != n and i < maxIterations):
+        if (x**2 > n):
+            x -= shift
+        else:
+            x += shift
+
+        shift /= 2
+        i += 1
+
+    return x
